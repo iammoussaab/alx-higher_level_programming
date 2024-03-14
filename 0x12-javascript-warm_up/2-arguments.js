67ee0ea042c241f0d23e1e770fb240c965433225
Moussaab el mahraoui <90398353+iammoussaab@users.noreply.github.com>
2024-03-14 15:33:32 +0000

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const numArgs = process.argv.length;
-if (numArgs === 2) {
-    console.log("No argument");
-} else if (numArgs === 3) {
-    console.log("Argument found");
-} else {
-    console.log("Arguments found");
-}
