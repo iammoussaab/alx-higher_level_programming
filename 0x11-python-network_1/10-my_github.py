439080e5d4bd499f50f54329174458a95061c798
Moussaab el mahraoui <90398353+iammoussaab@users.noreply.github.com>
2024-04-01 19:43:14 +0000

    time hell everything

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
 """A script that:
-- takes your GitHub credentials (username and password)
+- takes your GitHub credentials (username and personal access token)
 - uses the GitHub API to display your id
 """
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
-
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
