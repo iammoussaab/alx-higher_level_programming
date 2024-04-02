836a4ba7b816653ccf235c25d2efa81efbffc189
Moussaab el mahraoui <90398353+iammoussaab@users.noreply.github.com>
2024-04-02 06:17:04 +0000

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
+"""
+Fetch URL using requests package:
+This Python script fetches https://alx-intranet.hbtn.io/status using requests
+"""
 import requests
 
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    response = requests.get(url)
+
+def fetch_url():
+    """This method fetchs https://alx-intranet.hbtn.io/status using requests"""
+   res = requests.get("https://alx-intranet.hbtn.io/status")
+   return res.text
+
+
+def main():
+    """Main Entry - This is the main entry of the program"""
+    body = fetch_url()
     print("Body response:")
-    print("\t- type:", type(response.text))
-    print("\t- content:", response.text)
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+
+
+if __name__ == "__main__":
+    main()
