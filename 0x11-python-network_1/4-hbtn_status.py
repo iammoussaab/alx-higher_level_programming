bbc2cd8221b281bfd28af466882540970a85e9aa
iammoussaab <iammoussaab@gmail.com>
2024-04-01 23:22:31 -0700

    againsdsadfc

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status using requests"""
+"""
+Fetches https://alx-intranet.hbtn.io/status using the requests package
+and displays the body of the response in a specific format.
+"""
 import requests
 
-
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
+
