581a5c11166ea07be117223e9d6fbc795c77d33e
Moussaab el mahraoui <90398353+iammoussaab@users.noreply.github.com>
2024-04-02 05:57:42 +0000

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,9 +9,6 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    """takes in a URL, sends a request to the URL and displays
-    the value of the X-Request-Id variable found in the header
-    of the response"""
     with urllib.request.urlopen(argv[1]) as response:
         html_id = response.info().get('X-Request-Id')
         print(html_id)
